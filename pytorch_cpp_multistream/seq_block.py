6a8d80a078c1d92c84ca4cdf2c986d678e7fe7de
sandeepkumar-skb <sandeep.skb@uber.com>
2020-11-05 22:36:38 -0500

    making the network long and slim

--- pytorch_cpp_multistream/seq_block.py
+++ pytorch_cpp_multistream/seq_block.py
@@ -5,15 +5,25 @@ class Net(nn.Module):
     def __init__(self, features, num_layers):
         super().__init__()
         layers = [nn.Linear(features, features) for _ in range(num_layers)]
-        self.fc_block = nn.Sequential(*layers)
+        self.fc_block = nn.Sequential(*layers, 
+                                      nn.ReLU())
 
     def forward(self, inp):
-        return self.fc_block(inp)
-
+        out = self.fc_block(inp)
+        out = self.fc_block(out)
+        out = self.fc_block(out)
+        out = self.fc_block(out)
+        out = self.fc_block(out)
+        out = self.fc_block(out)
+        out = self.fc_block(out)
+        out = self.fc_block(out)
+        out = self.fc_block(out)
+        out = self.fc_block(out)
+        return out
 
 if __name__ == "__main__":
-    net = torch.jit.script(Net(512, 10))
-    inp = torch.randn((512,512), device="cuda")
+    net = torch.jit.script(Net(64, 10))
+    inp = torch.randn((64,64), device="cuda")
     net.cuda().eval()
     net(inp)
     torch.jit.save(net, "seq_block.pth")
